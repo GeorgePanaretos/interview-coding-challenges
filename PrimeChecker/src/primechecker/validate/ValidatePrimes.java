ef1834b365f1fbee73ace8e7a0c1c288a91f3c80
George Panaretos <georgepanaretos.cac@gmail.com>
2023-08-21 01:07:30 +0200

    Fix previous solution

--- PrimeChecker/src/primechecker/validate/ValidatePrimes.java
+++ PrimeChecker/src/primechecker/validate/ValidatePrimes.java
@@ -1,17 +1,16 @@
 package primechecker.validate;
 
 public class ValidatePrimes {
-    
-    public ValidatePrimes() {}
-    
-    //Validate the current number if it is Prime
-    public static boolean validatePrimes(int number){
-       for (int i = 2; i < (number / 2); i++) {
-        if (number% i == 0) {
-         return false;
+    public static boolean validatePrimes(int number) {
+        if (number < 2) {
+            return false;
         }
-       }
-       return true;
+
+        for (int divisor = 2; divisor <= Math.sqrt(number); divisor++) {
+            if (number % divisor == 0) {
+                return false;
+            }
+        }
+        return true;
     }
-   
 }
